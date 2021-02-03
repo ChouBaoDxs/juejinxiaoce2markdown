35bac5799411f4eede2fe1426c3d2f5210958689
DESKTOP-41BCVNQ\choubaodxs <826842697@qq.com>
2021-02-03 23:29:33 +0800

    重写完成，直接使用 requests，淘汰 selenium

--- README.md
+++ README.md
@@ -5,18 +5,54 @@ Python3 将掘金小册保存为本地 markdown （会处理图片）
 Python3 将掘金小册保存为本地 markdown
 
 ## 使用指南
-
+1. 安装依赖
+    ```shell
+    pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+    ```
+2. 根据实际情况修改 config.yml
+    ```yaml
+    sessionid: "{这里填写自己的sessionid}" # 填写 cookie 里的 sessionid
+    book_ids: # 要爬取的小册id(在url里可以找到)，必须是上面账号已购的小册
+    #  - 6844733722936377351 # 深入理解 RPC : 基于 Python 自建分布式高并发 RPC 服务
+    #  - 6844733712102326279 # 大厂 H5 开发实战手册
+    #  - 6844733730678898702 # 基于 Go 语言构建企业级的 RESTful API 服务
+    #  - 6844733718335062030 # 基于 Python 轻松自建 App 服务器
+      - 6844733813021491207 # 深入浅出TypeScript：从基础知识到类型编程
+      - 6844733800300150797 # 前端算法与数据结构面试：底层逻辑解读与大厂真题训练
+      - 6897616008173846543 # 从 0 到 1 实现一套 CI/CD 流程
+    save_dir: "book" # 保存小册的目录，默认会在当前目录下新建一个 book 目录
+    ```
+3. 运行主程序：`python3 main.py`
+4. 爬取结果预览：
+```
+├── book
+│   └── 深入浅出TypeScript：从基础知识到类型编程
+│       ├── 1-小册食用指南.md
+│       ├── 2-到底为什么要学习 TypeScript？.md
+│       └── img
+│           ├── 1
+│           │   └── 1
+│           └── 2
+│               ├── 1
+│               ├── 2
+│               ├── 3
+│               ├── 4
+│               └── 5
+├── config.yml
+├── main.py
+└── requirements.txt
+```
 # 分支二：crawl_by_chromedriver
 Python3 + Selenium 将掘金小册保存为本地 markdown （会处理图片）
 
 注意：该分支使用到了 Selenium + Chromedriver，配置起来比较繁琐，不推荐使用也不再维护
 
 ## 使用指南
-### 下载chromedriver
+1. 下载chromedriver
 - chromedriver下载地址：http://npm.taobao.org/mirrors/chromedriver/
 - 注意：请根据你的chrome版本和操作系统版本下载对应的chromedriver
 
-### 安装Python依赖
+2. 安装Python依赖
 - 建议使用pipenv或virtualenvwrapper之类的虚拟环境管理工具创建一个干净的Python3环境
 
 - 安装依赖：
@@ -24,7 +60,7 @@ Python3 + Selenium 将掘金小册保存为本地 markdown （会处理图片）
   pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
   ```
 
-### 根据实际情况修改config.yml
+3. 根据实际情况修改config.yml
 ```yaml
 juejin_account: "juejin_account" # 掘金登录账号
 juejin_password: "juejin_password" # 掘金登录密码
@@ -36,12 +72,12 @@ chromedriver_path: "chromedriver_mac" # chromedriver可执行文件的绝对路�
 save_dir: "./book" # 保存小册的目录，默认为当前目录
 ```
 
-### 开始爬取
-```python
+4. 开始爬取
+```
 python main.py
 ```
 
-### 爬取结果预览
+5. 爬取结果预览
 ```
 ├── LICENSE
 ├── README.md
@@ -64,6 +100,6 @@ python main.py
 └── requirements.txt
 ```
 
-### 后续问题
+## 后续问题
 **20210120**
 发现输入账号密码+拖动验证码无法登录，建议采用微信扫码登录
--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-import yaml
-# update at: 2020-01-13
-# python: 3.6.3
-
+import logging
 import os
-import platform
-import random
 import re
-import time
 import traceback
+from urllib.request import urlretrieve
+import yaml
 
 import html2text
-from PIL import Image
 import requests
-from selenium import webdriver
-from tqdm import tqdm
 
-
-def random_sleep(min=1, max=3):
-    time.sleep(random.random() * (max - min) + min)
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)7s: %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def makedirs(path):
@@ -25,221 +18,127 @@ def makedirs(path):
         os.makedirs(path)
 
 
-def webp_to_jpg(input_name, output_name):
-    im = Image.open(input_name)
-    if im.mode == "RGBA":
-        im.load()
-        background = Image.new("RGB", im.size, (255, 255, 255))
-        background.paste(im, mask=im.split()[3])
-        im = background
-    im.save('{}.jpg'.format(output_name), 'JPEG')
-    os.remove(input_name)
+def clear_slash(s: str) -> str:
+    return s.replace('\\', '').replace('/', '').replace('|', '')
 
 
-class Helper:
-    JUEJIN_HOST = 'https://juejin.im/'
-    JUEJIN_BOOK_URL_FORMAT = 'https://juejin.im/book/{book_id}'
+class Juejinxiaoce2Markdown:
+    img_pattern = re.compile(r'!\[.*?\]\((.*?)\)', re.S)
 
-    multiple_blank_lines_pattern = re.compile('\n+', re.S)
-    image_src_pattern = re.compile(r'data-src="(.*?)"', re.S)
-    download_image_headers = {
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-encoding': 'gzip, deflate, br',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'cache-control': 'max-age=0',
-    }
+    def __init__(self, config: dict):
+        logger.info(config)
+        pwd = os.path.dirname(os.path.abspath(__file__))
+        default_save_dir = os.path.join(pwd, 'book')
+        sessionid: str = config['sessionid']
+        self.book_ids: list = config['book_ids']
+        self.save_dir: str = config.get('save_dir', default_save_dir)
+        self.request_headers = {
+            'cookie': f'sessionid={sessionid};',
+        }
+        makedirs(self.save_dir)
 
-    def __init__(self, juejin_account, juejin_password, book_ids, save_dir, chromedriver_path):
-        self.juejin_account = juejin_account
-        self.juejin_password = juejin_password
-        self.book_ids = [book_ids] if isinstance(book_ids, str) else book_ids
-        self.save_dir = save_dir
-        self.chromedriver_path = chromedriver_path
+    def get_section_res(self, section_id):
+        url = f'https://api.juejin.cn/booklet_api/v1/section/get'
+        data = {
+            'section_id': str(section_id)
+        }
+        res = requests.post(url, headers=self.request_headers, json=data)
+        # logger.info(res.text)
+        return res
 
-    def init_chrome_options(self, proxy=None):
-        """
-        初始化启动参数
-        """
-        chrome_options = webdriver.ChromeOptions()
-        # chrome_options.add_argument('--headless')   # 设置成无界面
-        # chrome_options.add_argument('--no-sandbox')   # 禁用沙盒 解决linux下root用户无法运行的问题
+    def get_book_info_res(self, book_id) -> requests.Response:
+        url = f'https://api.juejin.cn/booklet_api/v1/booklet/get'
+        data = {
+            'booklet_id': str(book_id)
+        }
+        res = requests.post(url, headers=self.request_headers, json=data)
+        # logger.info(res.text)
+        return res
 
-        if proxy:
-            chrome_options.add_argument(f'--proxy-server={proxy}')
+    @classmethod
+    def save_markdown(cls, markdown_file_path, section_img_dir, markdown_relative_img_dir, markdown_str):
+        img_urls = re.findall(cls.img_pattern, markdown_str)
+        for img_index, img_url in enumerate(img_urls):
+            new_img_url: str = img_url.replace('\n', '')
+            if new_img_url.startswith('//'):
+                new_img_url = f'https:{new_img_url}'
+            try:
+                suffix = os.path.splitext(new_img_url)[-1]
+                img_file_name = f'{img_index + 1}{suffix}'
+                md_relative_img_path = os.path.join(markdown_relative_img_dir, img_file_name)
+                img_save_path = os.path.join(section_img_dir, img_file_name)
+                urlretrieve(new_img_url, img_save_path)
+                markdown_str = markdown_str.replace(img_url, md_relative_img_path)
+            except Exception as e:
+                logger.error({
+                    'msg': '处理图片失败',
+                    'img_url': new_img_url,
+                    'e': repr(e),
+                    'traceback': traceback.format_exc(),
+                    'markdown_relative_img_dir': markdown_relative_img_dir
+                })
+        with open(markdown_file_path, 'w', encoding='utf8') as f:
+            f.write(markdown_str)
 
-        # 设置成不加载图片
-        # prefs = {'profile.managed_default_content_settings.images': 2}
-        # chrome_options.add_experimental_option('prefs', prefs)
+    def deal_a_book(self, book_id):
+        log_data = {
+            'book_id': book_id,
+            'msg': '开始处理小册'
+        }
+        logger.info(log_data)
 
-        self.chrome_options = chrome_options
-
-    def init_driver(self):
-        self.driver = webdriver.Chrome(
-            executable_path=self.chromedriver_path,
-            chrome_options=self.chrome_options
-        )
-        self.driver.implicitly_wait(10)  # 设置隐式等待时间为10秒
-
-    def login_juejin(self):
-        """
-        打开浏览器并登陆掘金
-        """
-        self.driver.get(self.JUEJIN_HOST)
-        self.driver.find_element_by_class_name('login-button').click()
-        if all([self.juejin_account, self.juejin_password]):
-            # 点击其他登录方式
-            self.driver.find_element_by_css_selector('.prompt-box .clickable').click()
-            random_sleep()
-
-            self.driver.find_element_by_name('loginPhoneOrEmail').send_keys(self.juejin_account)
-            random_sleep()
-            self.driver.find_element_by_name('loginPassword').send_keys(self.juejin_password)
-            random_sleep()
-            self.driver.find_element_by_css_selector('.panel .btn').click()
-            random_sleep()
-
-            # 这里需要滑动验证码
-            print('请滑动验证码...，验证码无法登录时请改用微信扫码登录')
-            time.sleep(20)
-        else:
-            print('你未设置账号密码，请手动输入进行登陆...')
-            time.sleep(20)
-
-    def close_useless_panel(self):
-        try:
-            [_.click() for _ in self.driver.find_elements_by_css_selector('.ion-close')]
-        except:
-            pass
-
-    def deal_book(self, book_id):
-        """
-        处理书籍
-        """
-        book_url = self.JUEJIN_BOOK_URL_FORMAT.format(book_id=book_id)
-        random_sleep()
-        self.driver.get(book_url)
-        random_sleep()
-        book_name = self.driver.find_element_by_css_selector('.title-line .title').text
-        book_name = book_name.replace('/', '')  # 书籍名称可能带有斜杆
-        save_book_dir = os.path.join(self.save_dir, book_name)
-        makedirs(save_book_dir)
-
-        img_dir = os.path.join(save_book_dir, 'img')
+        res = self.get_book_info_res(book_id)
+        res_json = res.json()
+        book_title = res_json['data']['booklet']['base_info']['title']
+        section_list = res_json['data']['sections']
+        book_title = clear_slash(book_title)
+        logger.info({'book_title': book_title})
+        book_save_path = os.path.join(self.save_dir, book_title)
+        img_dir = os.path.join(book_save_path, 'img')
         makedirs(img_dir)
 
-        sections = self.driver.find_elements_by_css_selector('.section.section-link')
-        sections_pbar = tqdm(range(len(sections)))
-        self.close_useless_panel()
-        for i in sections_pbar:
-            sections = self.driver.find_elements_by_css_selector('.section.section-link')
+        section_id_list = [e['section_id'] for e in section_list]
 
-            current_section = sections[i]
-            while True:
-                try:
-                    section_title = current_section.find_element_by_class_name('title').text
-                except:
-                    random_sleep()
-                else:
-                    break
-            while True:
-                try:
-                    current_section.click()
-                    if i == 0:  # 第一章页面切换可能要一点时间，稍微等一等
-                        random_sleep()
-                except:
-                    random_sleep()
-                    self.close_useless_panel()
-                    self.driver.execute_script('window.scrollBy(0, 244)')
-                else:
-                    break
+        section_count = len(section_id_list)
+        for index, section_id in enumerate(section_id_list):
+            section_order = index + 1
+            logger.info({
+                '进度': f'{section_order}/{section_count}',
+                'msg': '处理 section',
+                'section_id': section_id
+            })
+            res = self.get_section_res(section_id)
+            res_json = res.json()
+            section_title = res_json['data']['section']['title']
+            section_title = clear_slash(section_title)
+            markdown_html_str = res_json['data']['section']['content']
+            markdown_str = html2text.html2text(markdown_html_str)
+            # markdown_str = res_json['data']['section']['markdown_show']
+            markdown_file_path = os.path.join(book_save_path, f'{section_order}-{section_title}.md')
+            section_img_dir = os.path.join(img_dir, f'{section_order}')
+            makedirs(section_img_dir)
+            markdown_relative_img_dir = os.path.join('img', f'{section_order}')
+            self.save_markdown(markdown_file_path, section_img_dir, markdown_relative_img_dir, markdown_str)
 
-            section_title = section_title.replace('/', '')  # 替换章节名中的斜杆，会影响路径
-
-            sections_pbar.set_description(f'正在处理第{i+1}章-{section_title}')
-            section_order = i + 1
-            section_markdown_save_path = os.path.join(save_book_dir, f'{section_order}-{section_title}.md')
-            if os.path.exists(section_markdown_save_path):
-                print(f'{section_markdown_save_path}已经存在，跳过处理...')
-                continue
-            section_img_save_dir = os.path.join(img_dir, str(section_order))
-            makedirs(section_img_save_dir)
-            section_content_html = self.driver.find_element_by_css_selector('.article-content').get_attribute('innerHTML')
-            try:
-                self.covert_html_to_markdown(section_order, section_markdown_save_path, section_img_save_dir, section_content_html)
-            except Exception as e:
-                print(f'转换markdown出错，请手动处理...:{section_title}-{e}')
-                traceback.print_exc()
-
-    def covert_html_to_markdown(self, section_order, section_markdown_save_path, section_img_save_dir, section_content_html):
-        """
-        将html转为markdown，并且下载图片
-        """
-        img_urls = re.findall(self.image_src_pattern, section_content_html)
-        imgs_pbar = tqdm(img_urls)
-        for img_url in imgs_pbar:
-            imgs_pbar.set_description(f'正在处理{img_url}')
-            try:
-                section_content_html = self.download_img(img_url, section_order, section_img_save_dir, section_content_html)
-            except Exception as e:
-                print(f'下载图片出错，请手动处理...:{img_url}-{e}')
-                traceback.print_exc()
-
-        markdown = html2text.html2text(section_content_html)
-        markdown = re.sub(self.multiple_blank_lines_pattern, '\n', markdown)
-        with open(section_markdown_save_path, 'w', encoding='utf8') as f:
-            f.write(markdown)
-
-    def download_img(self, url, section_order, section_img_save_dir, section_content_html):
-        """
-        下载图片并转化(普通图片是webp，要转jpg，还有可能是gif)，还要替换html中的图片链接
-        """
-        filename = f'{int(time.time() * 1000)}'
-        pattern = re.compile('data-src="{img_url}.*?</svg>'.format(img_url=url.replace('.', '\.').replace('?', '\?')), re.S)
-        file_save_path = os.path.join(section_img_save_dir, filename)
-        if url.startswith('//'):
-            url = 'https:' + url
-        res = requests.get(url, headers=self.download_image_headers)
-        with open(file_save_path, 'wb') as f:
-            f.write(res.content)
-        res_content_type = res.headers.get('content-type')
-        if res_content_type == 'image/webp':
-            webp_to_jpg(file_save_path, file_save_path)
-            re_sub_str = f'img/{section_order}/{filename}.jpg'
-        elif res_content_type == 'image/gif':
-            os.rename(file_save_path, f'{file_save_path}.gif')
-            re_sub_str = f'img/{section_order}/{filename}.gif'
-        elif res_content_type == 'image/png':
-            os.rename(file_save_path, f'{file_save_path}.png')
-            re_sub_str = f'img/{section_order}/{filename}.png'
-        else:
-            re_sub_str = f'img/{section_order}/{filename}'
-            print(f'未知的图片类型，请手动处理:{url}')
-        section_content_html = re.sub(pattern, 'src="' + re_sub_str, section_content_html)
-        return section_content_html
+        log_data['msg'] = '处理完成'
+        logger.info(log_data)
 
     def main(self):
-        self.init_chrome_options()
-        self.init_driver()
-
-        self.login_juejin()
-
-        books_pbar = tqdm(self.book_ids)
-        for i, book_id in enumerate(books_pbar):
-            books_pbar.set_description(f'正在处理第{i+1}本书-{book_id}')
+        for book_id in self.book_ids:
             try:
-                self.deal_book(book_id)
+                self.deal_a_book(book_id)
             except Exception as e:
-                print(f'爬取书籍出错：{book_id}')
-                traceback.print_exc()
-
-        input('处理完成，按任意键退出!')
-        self.driver.quit()
+                log_data = {
+                    'book_id': book_id,
+                    'e': repr(e),
+                    'traceback': traceback.format_exc(),
+                    'msg': '处理小册出错'
+                }
+                logger.error(log_data)
 
 
 if __name__ == '__main__':
     with open('config.yml', encoding='utf-8') as f:
         config = yaml.safe_load(f)
-    config['save_dir'] = config['save_dir'] or os.path.dirname(os.path.abspath(__file__))
-    helper = Helper(**config)
+    helper = Juejinxiaoce2Markdown(config)
     helper.main()
